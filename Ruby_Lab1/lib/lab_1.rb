7695ff4df8b531f910e9c91b64069c731318c0a1
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-09-19 13:58:20 +0300

    Added 1st lab

---
+++ Ruby_Lab1/lib/lab_1.rb
@@ -0,0 +1,13 @@
+puts 'Input x'
+$x=gets.chomp.to_f
+
+puts 'Input a'
+$a=gets.chomp.to_f
+
+puts 'Input fi, gamma'
+$fi=gets.chomp.to_f
+$ga=gets.chomp.to_f
+
+
+$r= Math.exp(Math.sin(Math::PI**2).abs)*((Math.log10(1+Math.cos($a)**2)+Math.tan(Math.sin($x))+4.8*(10**1.3))/5*Math.sqrt($fi+$ga))
+puts 'Result: '+ $r.to_s
