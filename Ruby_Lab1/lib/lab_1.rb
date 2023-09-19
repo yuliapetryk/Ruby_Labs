01bb32073ae1a06c834be6be2efbe20766336348
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-09-19 15:23:32 +0300

    Modified 1st lab

--- Ruby_Lab1/lib/lab_1.rb
+++ Ruby_Lab1/lib/lab_1.rb
@@ -9,5 +9,5 @@ $fi=gets.chomp.to_f
 $ga=gets.chomp.to_f
 
 
-$r= Math.exp(Math.sin(Math::PI**2).abs)*((Math.log10(1+Math.cos($a)**2)+Math.tan(Math.sin($x))+4.8*(10**1.3))/5*Math.sqrt($fi+$ga))
+$r= Math.exp(Math.sin(Math::PI**2).abs)*((Math.log(1+Math.cos($a)**2)+Math.tan(Math.sin($x))+4.8*(10**1.3))/5*Math.sqrt($fi+$ga))
 puts 'Result: '+ $r.to_s
